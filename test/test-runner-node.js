99652fa5e9ba0dd8179698ed2fe936ffeaee6796
Mike Koss <mckoss@startpad.org>
2011-04-07 14:35:46 -0700

    handle relative paths - don't modify require.paths

--- test/test-runner-node.js
+++ test/test-runner-node.js
@@ -1,17 +1,14 @@
 #!/usr/bin/env node
 
-require.paths.unshift('.');
-require.paths.unshift('../src');
-global.namespace = require('namespace.js').namespace;
+global.namespace = require('../src/namespace.js').namespace;
 
-require('types.js');
-require('funcs.js');
-require('qunit.js');
+require('../src/types.js');
+require('../src/funcs.js');
+require('./qunit.js');
 
-var funcs = namespace.lookup('org.startpad.funcs');
 var types = namespace.lookup('org.startpad.types');
-
 var ut = namespace.lookup('com.jquery.qunit');
+
 var moduleInfo, testInfo;
 var testNumber = 1;
 
@@ -26,7 +23,7 @@ types.extend(ut.QUnit, {
 
     testDone: function (info) {
         console.log(moduleInfo.name + ': ' + info.name +
-                    '(' + info.passed + '/' + info.total + ' pass)');
+                    ' (' + info.passed + '/' + info.total + ' pass)');
     },
 
     log: function (info) {
@@ -36,6 +33,6 @@ types.extend(ut.QUnit, {
     }
 });
 
-require('test-namespace.js');
-require('test-types.js');
-require('test-funcs.js');
+require('./test-namespace.js');
+require('./test-types.js');
+require('./test-funcs.js');
